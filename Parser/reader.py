f834aa93a47b15f77c58a12345c643fb9339d708
Claes Mortensen <claesm1999@gmail.com>
2023-02-27 10:57:25 +0100

    made reader a class instead :>

--- Parser/reader.py
+++ Parser/reader.py
@@ -2,13 +2,19 @@ import glob
 import os
 from systemd import journal
 
-dataPath = ""
-filename = ""
-fileExtension = ""
 
-# traversal of file directory
-os.chdir(dataPath)
-for file in glob.glob(filename):
-    if file.endswith(fileExtension):
-        # do some magic parsing
-        pass
+class Reader:
+    dataPath = ""
+    fileName = ""
+    fileExtension = ""
+
+    def __init__(self, dataPath, fileName, fileExtension) -> None:
+        self.dataPath = dataPath
+        self.fileName = fileName
+        self.fileExtension = fileExtension
+
+    os.chdir(dataPath)
+    for file in glob.glob(fileName):
+        if file.endswith(fileExtension):
+            # do some parsing magic
+            pass
