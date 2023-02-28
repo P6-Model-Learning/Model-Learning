700ad39d6f1e536c2f2d488c7fd94922d552d029
Claes Mortensen <claesm1999@gmail.com>
2023-02-28 13:19:28 +0100

    added functionality to reader class: finds all syslogs in data directory of type systemd

--- Parser/reader.py
+++ Parser/reader.py
@@ -1,21 +1,35 @@
 import os
 import fnmatch
+from systemd import journal
 
 
 class Reader:
     def __int__(self):
         pass
 
-    def readJorunals(self):
-        pass
+    def read_journals(self) -> list:
+        syslog_list = []
+        for root, dirs, files in os.walk(os.pardir):
+            path = root.split(os.path.sep)
+            for file in files:
+                if file.startswith('system.journal') and file.endswith('.journal'):
+                    j = journal.Reader(path=root)
+                    j.get_next(skip=1)
+                    j.log_level(level=7)
+                    j.add_match("SYSLOG_IDENTIFIER=systemd")
+                    for entry in j:
+                        syslog_list.append(entry)
+                        pass  # TODO: parsing <3
+        return syslog_list
 
-    def printDir(self):
+    def print_dir(self):
         for root, dirs, files in os.walk(os.pardir):
             path = root.split(os.sep)
-            print((len(path) - 1) * '---', os.path.basename(root))
             for file in files:
                 if file.startswith("system.journal") and file.endswith(".journal"):
+                    print((len(path) - 1) * '---', os.path.basename(root))
                     print((len(path) - 1) * '   ' + ' |__', file)
 
+
 reader = Reader()
-reader.printDir()
+reader.read_journals()
