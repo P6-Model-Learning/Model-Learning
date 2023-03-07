889a753219c3adff163747cd53268a6448e06cd8
Sigurd <j.skadborg00@gmail.com>
2023-03-07 13:40:09 +0100

    parse traces into java program

--- src/main/java/aal/syslearner/AalSysLearner.java
+++ src/main/java/aal/syslearner/AalSysLearner.java
@@ -1,11 +1,18 @@
 package aal.syslearner;
 
+import org.json.simple.parser.ParseException;
+
 import java.io.IOException;
 
 public class AalSysLearner
 {
     public static void main(String args[]) throws IOException {
         DataController dc = new DataController();
-        dc.getTraces();
+        try {
+            dc.getTraces();
+        }
+        catch (ParseException | IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/main/java/aal/syslearner/DataController.java
+++ src/main/java/aal/syslearner/DataController.java
@@ -1,32 +1,47 @@
 package aal.syslearner;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class DataController {
 
-    public List<Trace> getTraces() throws IOException {
+    public List<Trace> getTraces() throws IOException, ParseException {
+        return getTraces(null);
+    }
+    public List<Trace> getTraces(String[] args) throws IOException, ParseException {
 
         JSONParser parser = new JSONParser();
+        JSONArray a;
+        Process p = Runtime.getRuntime().exec("python __main__.py");
 
-        try {
-            Process p = Runtime.getRuntime().exec("python __main__.py");
+        a = (JSONArray) parser.parse(new FileReader("out.json"));
 
-
-            JSONArray a = (JSONArray) parser.parse(new FileReader("out.json"));
-        } catch (IOException exception) {
-            System.out.println(exception.getMessage());
-            exception.printStackTrace();
-        }
-
-        return new ArrayList<>();
+        return ExtractTraces(a);
     }
+
+    private static List<Trace> ExtractTraces(JSONArray a) {
+        ArrayList<Trace> traces = new ArrayList<>();
+        for (Object b : a) {
+            JSONArray board = (JSONArray) b;
+            for (Object t : board) {
+                JSONArray trace = (JSONArray) t;
+                List<Event> events = new ArrayList<>();
+                for (Object e : trace) {
+                    JSONObject event = (JSONObject) e;
+                    String eventMessage = (String) event.get("MESSAGE");
+                    events.add(new Event(eventMessage));
+                }
+                traces.add(new Trace(events));
+            }
+        }
+        return traces;
+    }
+
 }
