1559f84b539a0f6e0da6874b9e95c045def67e51
Victor Doré Hansen <vdha20@student.aau.dk>
2023-03-17 13:40:29 +0100

    Commands now echoed + TODOs added

--- Parser/getJournalsFromBoard.sh
+++ Parser/getJournalsFromBoard.sh
@@ -4,10 +4,15 @@ output_dir=OutputJournals/
 log_dir=/var/log/journal/
 ssh_password=root
 
-#Get latest modified subdirectory of journal
+#TODO: change to get the list of /journal/ subdirectories sorted by modification date
+echo SSH into BBB to get latest modified journal subdirectory
 dir_to_copy=`sshpass -p $ssh_password ssh root@$bbb_ip ls $log_dir -At | head -n 1`
 
+#TODO: Get the n latest modified journal files - Ignore the first instance of system.journal
+
+echo Making output directory
 mkdir $output_dir
 
-#copy from BBB to local system
+#TODO: Iterate through the list of n latest modified journal files
+echo Copying files from BBB into local system
 sshpass -p $ssh_password scp -r root@$bbb_ip:$log_dir$dir_to_copy $output_dir
