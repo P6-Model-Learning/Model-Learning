a2d104166b4b22df9d46a3a4dced6e45a4cd3a23
Victor Doré Hansen <vdha20@student.aau.dk>
2023-03-17 12:54:24 +0100

    Now provides password as part of script

--- Parser/getJournalsFromBoard.sh
+++ Parser/getJournalsFromBoard.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-output_dir=OutputJournals/
 bbb_ip=192.168.1.158
+output_dir=OutputJournals/
 log_dir=/var/log/journal/
-dir_to_copy=`ssh root@$bbb_ip ls $log_dir -At | head -n 1`
+ssh_password=root
+
+#Get latest modified subdirectory of journal
+dir_to_copy=`sshpass -p $ssh_password ssh root@$bbb_ip ls $log_dir -At | head -n 1`
 
 mkdir $output_dir
 
 #copy from BBB to local system
-scp -r root@$bbb_ip:$log_dir$dir_to_copy $output_dir
+sshpass -p $ssh_password scp -r root@$bbb_ip:$log_dir$dir_to_copy $output_dir
