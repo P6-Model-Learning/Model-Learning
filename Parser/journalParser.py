69239c360833ca32769bcc6ec0efa2260079ffc0
Claes Mortensen <claesm1999@gmail.com>
2023-03-21 10:50:28 +0100

    added check for start string in journals

--- Parser/journalParser.py
+++ Parser/journalParser.py
@@ -3,22 +3,27 @@ from systemd import journal
 import json
 
 class JournalParser:
+    startStr = ""
+    endStr = ""
+
     def getBoards(self):
         pass
 
     def parse(self):
         data = []
-        for root, dirs, files in os.getcwd:
+        startTime = None
+        for root, dirs, files in os.walk(os.getcwd()):
             for file in files:
-                if file.endswitch('.journal'):
+                if file.endswith('.journal'):
                     trace = []
                     j = journal.Reader(path=root)
                     j.get_next(skip=1)
                     j.log_level(level=7)
                     for entry in j:
-                        trace.append(entry)
+                        if entry['MESSAGE'] == self.startStr:
+                            trace.append(entry)
                     data.append(trace)
         return data
     
     def parseToJSON(self, data):
-        return json.dumps(data)
+        return json.dumps(data, sort_keys=True, default=str)
