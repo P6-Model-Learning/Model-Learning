d4f4e656a38182af11200c171fedcd95ad1359ad
Claes Berg Mortensen <45384716+HTHNLNDN@users.noreply.github.com>
2023-03-24 13:09:02 +0100

    Feature/new journal parsing (#12)
    * added more generic parsing for new journla files
    
    * added check for start string in journals
    
    * added some flags :)
    
    * fix for using specific branch

--- Parser/journalParser.py
+++ Parser/journalParser.py
@@ -2,14 +2,14 @@ import os
 from systemd import journal
 import json
 
-class JournalParser:
-    startStr = ""
-    endStr = ""
+class JournalParser:    
+    startStr = "Test start line inserted"
+    endStr = "Active journal rotated"
 
     def getBoards(self):
         pass
 
-    def parse(self):
+    def parse(self, board:str):
         data = []
         startTime = None
         for root, dirs, files in os.walk(os.getcwd()):
@@ -17,13 +17,56 @@ class JournalParser:
                 if file.endswith('.journal'):
                     trace = []
                     j = journal.Reader(path=root)
+                    if board != None: j.add_match("_HOSTNAME={board}".format(board = board))
                     j.get_next(skip=1)
                     j.log_level(level=7)
                     for entry in j:
                         if entry['MESSAGE'] == self.startStr:
+                            startTime = entry['_SOURCE_MONOTONIC_TIMESTAMP']
+                            trace.append(entry)
+                        elif startTime != None:
                             trace.append(entry)
                     data.append(trace)
-        return data
+                    startTime = None
+        return list(filter(None, data))
+    
+    def parseSimple(self, board:str):
+        data = []
+        start_time = None
+        for root, dirs, files in os.walk(os.getcwd()):
+            for file in files:
+                if file.endswith('.journal'):
+                    trace = []
+                    j = journal.Reader(path=root)
+                    if board != None: j.add_match("_HOSTNAME={board}".format(board = board))
+                    j.get_next(skip=1)
+                    j.log_level(level=7)
+                    for entry in j:
+                        if entry['MESSAGE'] == self.startStr:
+                            start_time = entry['_SOURCE_MONOTONIC_TIMESTAMP']
+                            trace.append({
+                                entry['_SOURCE_MONOTONIC_TIMESTAMP'],
+                                entry['MESSAGE'],
+                                entry['_HOSTNAME']
+                            })
+                        elif start_time != None:
+                            trace.append({
+                                entry['_SOURCE_MONOTONIC_TIMESTAMP'],
+                                entry['MESSAGE'],
+                                entry['_HOSTNAME']
+                            })
+                    data.append(trace)
+                    start_time = None
+        return list(filter(None, data))
+    
+    def getBoards(self) -> list:
+        boards = []
+
+        for root, dirs, files in os.walk(os.getcwd()):
+            if root not in boards and (os.path.basename(root)).endswith('.prevas.dk'):
+                boards.append(((os.path.basename(root))
+                               .split('-dut')[0], os.path.relpath(root, os.getcwd())))
+        return boards
     
     def parseToJSON(self, data):
         return json.dumps(data, sort_keys=True, default=str)
--- __main__.py
+++ __main__.py
@@ -6,11 +6,17 @@ parser = argparse.ArgumentParser(description="Print JSON from parsed syslog data
 parser.add_argument('-p','--prune', dest='prune', help='prune the data to remove trivial sequential entries', action="store_true")
 parser.add_argument('-i', '--init', dest='init', help='parse initial boot data for boards', action='store_true')
 parser.add_argument('-o', '--out', dest='out', help='output the json file at current working directory', action='store_true')
+parser.add_argument('-s','--simplify', dest='simplify', help='simplify the JSON object to only contain necessary keys', action='store_true')
+parser.add_argument('-b', '--board', dest='boards', help='only parse for one board type, add board name after -b/--board')
+parser.add_argument('--print', dest='printboards', help='prints all boards in data', action='store_true')
 
 args = parser.parse_args()
 
 journal = None
 prune = None
+
+data = None
+board = None
 
 #check for 'data type' wether init data or new data
 if args.init:
@@ -18,9 +24,25 @@ if args.init:
 else:
     journal = journalParser.JournalParser()
 
+boards = dict(journal.getBoards())
 
-boards = journal.getBoards()
-data = journal.parse()
+#check if non-parsing args
+if args.printboards:
+    print('Boards in data:')
+    for i in boards:
+        print(' * ', i)
+    quit()
+
+if args.boards and args.boards in boards:
+    board = args.boards
+elif args.boards and args.boards not in boards:
+    raise Exception("Boards was not found in data... Quitting.")
+    quit()
+
+if args.simplify:
+    data = journal.parseSimple(board)
+else:
+    data = journal.parse(board)
 
 if args.prune:
     prune = pruner.Pruner()
