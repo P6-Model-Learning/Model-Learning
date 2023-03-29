79e93a5e336ad5e04293a2dfb20a6ffa2cfd1a57
Claes Mortensen <claesm1999@gmail.com>
2023-03-29 12:49:00 +0200

    changed -o flag to do what it normally does

--- __main__.py
+++ __main__.py
@@ -5,7 +5,7 @@ parser = argparse.ArgumentParser(description="Print JSON from parsed syslog data
 
 parser.add_argument('-p','--prune', dest='prune', help='prune the data to remove trivial sequential entries', action="store_true")
 parser.add_argument('-i', '--init', dest='init', help='parse initial boot data for boards', action='store_true')
-parser.add_argument('-o', '--out', dest='out', help='output the json file at current working directory', action='store_true')
+parser.add_argument('-o', '--out', dest='out', help='rename the output, board name goes after -o')
 parser.add_argument('-s','--simplify', dest='simplify', help='simplify the JSON object to only contain necessary keys', action='store_true')
 parser.add_argument('-b', '--board', dest='boards', help='only parse for one board type, add board name after -b/--board')
 parser.add_argument('--print', dest='printboards', help='prints all boards in data', action='store_true')
@@ -49,5 +49,8 @@ if args.prune:
     prune.badKTail()
 
 if args.out:
+    with open('{args}.json'.format(args=args.out), 'w') as outfile:
+        outfile.write(journal.parseToJSON(data))
+else:
     with open('out.json', 'w') as outfile:
         outfile.write(journal.parseToJSON(data))
