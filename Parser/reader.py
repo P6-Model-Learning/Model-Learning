090b38a0e2f29e4be3b52bb2fe4671eb604f98c7
Claes Mortensen <claesm1999@gmail.com>
2023-04-04 09:38:04 +0200

    added simplified messages as a field

--- Parser/reader.py
+++ Parser/reader.py
@@ -1,6 +1,7 @@
 import os
 from systemd import journal
 import json
+import re
 
 class Reader:
     def getBoards(self) -> list:
@@ -51,6 +52,7 @@ class Reader:
                     if start_time == None: start_time = entry['__MONOTONIC_TIMESTAMP'][0]
                     entry['TIMEDELTA'] = (entry['__MONOTONIC_TIMESTAMP'][0] - start_time).total_seconds()
                     entry = {'MESSAGE':entry['MESSAGE'],
+                             'SIMPLE_MESSAGE':self.__pruneMSG(entry['MESSAGE']),
                              '_HOSTNAME':entry['_HOSTNAME'],
                              '__MONOTONIC_TIMESTAMP':entry['__MONOTONIC_TIMESTAMP'],
                              'TIMEDELTA':entry['TIMEDELTA']}
@@ -61,6 +63,7 @@ class Reader:
                     if board == None: board = entry['_HOSTNAME']
                     if start_time == None: start_time = entry['__MONOTONIC_TIMESTAMP'][0]
                     entry['TIMEDELTA'] = (entry['__MONOTONIC_TIMESTAMP'][0] - start_time).total_seconds()
+                    entry['SIMPLE_MESSAGE'] = self.__pruneMSG(entry['MESSAGE'])
                     trace.append(entry)
                 return trace, board
     
@@ -80,6 +83,7 @@ class Reader:
                     if start_time == None: start_time = entry['__MONOTONIC_TIMESTAMP'][0]
                     entry['TIMEDELTA'] = (entry['__MONOTONIC_TIMESTAMP'][0] - start_time).total_seconds()
                     entry = {'MESSAGE':entry['MESSAGE'],
+                             'SIMPLE_MESSAGE': self.__pruneMSG(entry['MESSAGE']),
                              '_HOSTNAME':entry['_HOSTNAME'],
                              '__MONOTONIC_TIMESTAMP':entry['__MONOTONIC_TIMESTAMP'],
                              'TIMEDELTA':entry['TIMEDELTA']}
@@ -90,9 +94,21 @@ class Reader:
                     if board == None: board = entry['_HOSTNAME']
                     if start_time == None: start_time = entry['__MONOTONIC_TIMESTAMP'][0]
                     entry['TIMEDELTA'] = (entry['__MONOTONIC_TIMESTAMP'][0] - start_time).total_seconds()
+                    entry['SIMPLE_MESSAGE'] = self.__pruneMSG(entry['MESSAGE'])
                     trace.append(entry)
                 return trace, board
     
+    def __pruneMSG(self, msg):
+        ip_regex = re.compile('[(](?:[0-9]{1,3}\.){3}[0-9]{1,3}[:][0-9]*[)]')
+        time_regex = re.compile('(in)\s[0-9]*.[0-9]*s.*')
+        sys_mode_regex = re.compile('(in system mode )[(](\s*([-]|[+])\w*\s*)*\s*(\w*[-]\w*[=]\w*)[)]')
+        if re.search(ip_regex, msg): 
+            msg = re.sub(ip_regex, '', msg)
+        if re.search(time_regex, msg): 
+            msg =  re.sub(time_regex, '', msg)
+        if re.search(sys_mode_regex, msg):
+            msg = re.sub(sys_mode_regex, '', msg)
+        return msg
 
     def parseToJSON(self, data):
         return json.dumps(data, sort_keys=True, default=str, indent=4)
