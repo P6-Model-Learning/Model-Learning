02d9bf408d9dc87aaa539e1900a09075959621ce
Claes Mortensen <claesm1999@gmail.com>
2023-05-08 06:56:51 +0200

    fixed check for start message. Added check for end message

--- Parser/reader.py
+++ Parser/reader.py
@@ -64,15 +64,15 @@ class Reader:
             start_time = None
             board = None
             trace = []
+            end_string = 'Received client request to rotate journal, rotating.'
             off = True
             j = journal.Reader(path=root)
             j.get_next(skip=1)
             j.log_level(level=7)
-            j.add_match("SYSLOG_IDENTIFIER=systemd")
             if simple:
                 for entry in j:
                     if entry['MESSAGE'] == start_string: off = False
-                    if off: pass
+                    if off: continue
                     if board == None: board = entry['_HOSTNAME']
                     if start_time == None: start_time = entry['__MONOTONIC_TIMESTAMP'][0]
                     entry['TIMEDELTA'] = (entry['__MONOTONIC_TIMESTAMP'][0] - start_time).total_seconds()
@@ -82,16 +82,18 @@ class Reader:
                              '__MONOTONIC_TIMESTAMP':entry['__MONOTONIC_TIMESTAMP'],
                              'TIMEDELTA':entry['TIMEDELTA']}
                     trace.append(entry)
+                    if entry['MESSAGE'] == end_string: off = True
                 return trace, board
             else:
                 for entry in j:
                     if entry['MESSAGE'] == start_string: off = False
-                    if off: pass
+                    if off: continue
                     if board == None: board = entry['_HOSTNAME']
                     if start_time == None: start_time = entry['__MONOTONIC_TIMESTAMP'][0]
                     entry['TIMEDELTA'] = (entry['__MONOTONIC_TIMESTAMP'][0] - start_time).total_seconds()
                     entry['SIMPLE_MESSAGE'] = self.__pruneMSG(entry['MESSAGE'])
                     trace.append(entry)
+                    if entry['MESSAGE'] == end_string: off = True
                 return trace, board
     
     def __pruneMSG(self, msg):
@@ -109,5 +111,21 @@ class Reader:
             msg = re.sub(sys_mode_regex, '', msg)
         return msg
 
+    def test_data(directory):
+        off = True
+        for root, dirs, files in os.walk(directory):
+            path = root.split(os.path.sep)
+            for file in files:
+                j = journal.Reader(path=root)
+                j.get_next(skip=1)
+                for entry in j:
+                    if entry['MESSAGE'] == 'Test started': off = False
+                    if off == True: continue
+                    print(entry['MESSAGE'])
+                    if entry['MESSAGE'] == 'Received client request to rotate journal, rotating.': off = True
+    
     def parseToJSON(self, data):
         return json.dumps(data, sort_keys=True, default=str, indent=4)
+
+x = Reader
+x.test_data(r'/home/claesm/Documents/code/sw6/bachelor_project/P6/p6-initdata/data/BBBTests.prevas.dk')
